ca92f308c195843594b62ee25c40673b728688d9
djole <dzordzcook@gmail.com>
2020-07-31 19:15:46 +0200

    Added edit/delete functionality for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :recipes
+  resources :recipes do resources :reviews
+end
   root 'recipes#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_31_144010) do
+ActiveRecord::Schema.define(version: 2020_07_31_154334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_07_31_144010) do
     t.string "recipe_img_content_type"
     t.bigint "recipe_img_file_size"
     t.datetime "recipe_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.integer "recipe_id"
   end
 
   create_table "users", force: :cascade do |t|
